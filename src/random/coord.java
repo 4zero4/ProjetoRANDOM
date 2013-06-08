41e517057a1b64b950d2b443c6c1b50bacf9226e
Qirion <gabriel.mfcorreia@gmail.com>
2013-06-08 16:56:55 -0300

    CSVPrinter funcionando!!

--- src/random/coord.java
+++ src/random/coord.java
@@ -8,7 +8,7 @@ public class coord {
 	public static double a = 179424361;
 	public static double b = 11;
 	public static double m = 1000000000;
-	public static double dNom = 100000;
+	public static double dNom = 1;
 	public static double[][] matriz;
 	
 	public static void main(String[] args){
@@ -19,6 +19,7 @@ public class coord {
 		msg("Programa de criação de coordenadas utilizando\nalgoritimos pseudo-randomicos");
 		n = (int)entrDouble("Entre com o número de estrelas a serem geradas");
 		matriz = new double[n][4];
+		
 		for(int linha=0;linha<matriz.length;linha++){
 			for(int coluna=0;coluna<4;coluna++){
 				if(coluna==0){// ID da estrela
@@ -36,27 +37,27 @@ public class coord {
 				}
 			}
 		}// loops!
-		CSVPrinter(matriz);
+		CSVPrinter();
+		msg("Pronto!");
 	}//main
 	//==============================================================
 	
-	public static void CSVPrinter(double[][] v){
-		int j=0;
-		int i=0;
+	
+	
+	public static void CSVPrinter(){
+		
 		try{
 			FileWriter writer = new FileWriter("test.csv");
-			for(; i < v.length; i++){
-				for (; j<v[0].length; j++){
-					writer.append(v[i][j]+"");
+			for(int linha = 0; linha < matriz.length; linha++){
+				for (int coluna = 0; coluna<matriz[0].length; coluna++){
+					writer.append(matriz[linha][coluna]+"");
 					writer.append(',');
 				}
-				writer.append(v[i][j-1]+"");
 				writer.append('\n');
-				writer.flush();
 			}
+			writer.flush();
 			writer.close();
-		}
-		catch(Exception e){
+		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}//CSVPrinter
